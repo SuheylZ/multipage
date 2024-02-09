6376f5dc152a1f89ec6a0fd49f5c1076b40c128f
suheyl <suheylz@hotmail.com>
2024-02-09 20:19:42 +0500

    index changes

--- src/modules/common/pages/index/index.tsx
+++ src/modules/common/pages/index/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '../index.css'
+import './index.css'
 import { useEvent, useInterval } from '../../hooks'
 
 function Index() {
@@ -18,6 +18,7 @@ function Index() {
 
   return (
     <>
+      {"Main Page"}
     </>
   )
 }
