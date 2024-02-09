948fdfed6be2cf8bafcab510462cb6a07f80368d
suheyl <suheylz@hotmail.com>
2024-02-09 20:14:58 +0500

    fixed folders to SPA structure

---
+++ src/modules/common/pages/index/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '../index.css'
+import { useEvent, useInterval } from '../../hooks'
+
+function Index() {
+  useEvent("online", (evt: Event) => {
+    console.log(`now online, ${evt.target}`)
+  })
+
+  useEvent("offline", (evt: Event) => {
+    console.log(`now offline, ${evt.target}`)
+  })
+
+  useInterval((x: number) => {
+    console.log(`${x}`)
+  }, 1000)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Index
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>,
+)
